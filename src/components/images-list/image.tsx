e374e210e2f01baba2f181a38fa0626909ccfa43
Andrey Kiselev <omega-faworit@yandex.ru>
2021-07-26 00:31:58 +0300

    feat: add lazy loading

--- src/components/images-list/image.tsx
+++ src/components/images-list/image.tsx
@@ -5,14 +5,19 @@ export const Image = () => {
   return (
     <div className="grid place-items-center">
       <article className="group first-letter:grid grid-flow-row items-center relative">
-        <img
-          src="http://placecorgi.com/300/300"
-          width="300"
-          height="300"
-          alt="Corgi"
-          className="hover:cursor-zoom-in"
-        />
-        <h2 className="text-2xl text-center">Corgi</h2>
+        <figure>
+          <img
+            src={`http://placecorgi.com/${
+              300 + Math.floor(Math.random() * 10)
+            }/${300 + Math.floor(Math.random() * 10)}`}
+            width="300"
+            loading="lazy"
+            height="300"
+            alt="Corgi"
+            className="hover:cursor-zoom-in"
+          />
+          <figcaption className="text-2xl text-center">Corgi</figcaption>
+        </figure>
         <button className="opacity-0 group-hover:opacity-100 absolute top-1 right-8 focus:opacity-100 focus:outline-none focus:ring rounded-full">
           <ZoomInIcon className="w-7 h-7" />
         </button>

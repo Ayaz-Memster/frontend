5c13622dd622b278cacb6e99ac0f5e3a61fe380a
Andrey Kiselev <omega-faworit@yandex.ru>
2021-07-26 12:50:20 +0300

    feat: add image sizing

--- src/components/images-list/image.tsx
+++ src/components/images-list/image.tsx
@@ -6,16 +6,27 @@ export const Image = () => {
     <div className="grid place-items-center">
       <article className="group first-letter:grid grid-flow-row items-center relative">
         <figure>
-          <img
-            src={`http://placecorgi.com/${
-              300 + Math.floor(Math.random() * 10)
-            }/${300 + Math.floor(Math.random() * 10)}`}
-            width="300"
-            loading="lazy"
-            height="300"
-            alt="Corgi"
-            className="hover:cursor-zoom-in"
-          />
+          <div className="w-[300px] h-[300px] overflow-hidden flex items-center justify-center hover:overflow-visible hover:scale-125">
+            <img
+              src={`http://placecorgi.com/${
+                500 + Math.floor(Math.random() * 100)
+              }/${300 + Math.floor(Math.random() * 100)}`}
+              width="300"
+              loading="lazy"
+              height="300"
+              alt="Corgi"
+              className="hover:cursor-zoom-in object-contain max-w-none"
+              onLoad={(event) => {
+                const img = event.target as HTMLImageElement;
+                const imgWidth = img.naturalWidth;
+                const imgHeight = img.naturalHeight;
+                const ratio = 300 / imgHeight;
+                const newWidth = imgWidth * ratio;
+                img.width = newWidth;
+                img.height = 300;
+              }}
+            />
+          </div>
           <figcaption className="text-2xl text-center">Corgi</figcaption>
         </figure>
         <button className="opacity-0 group-hover:opacity-100 absolute top-1 right-8 focus:opacity-100 focus:outline-none focus:ring rounded-full">

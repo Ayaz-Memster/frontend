2c9c378b568a4fe02f42331e1ad09da3d38a36d7
Andrey Kiselev <omega-faworit@yandex.ru>
2021-08-01 13:52:52 +0300

    fix: render add button into a portal

--- src/components/header/add-button.tsx
+++ src/components/header/add-button.tsx
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense, useState } from 'react';
 import { PlusCircleIcon } from '@heroicons/react/outline';
+import { Portal } from '@headlessui/react';
 
 const AddModal = lazy(() => import('../add-modal/add-modal'));
 
@@ -27,12 +28,14 @@ export const AddButton = ({ isMobile }: AddButtonProps) => {
           <PlusCircleIcon className="w-6 h-6 text-green-500" />
         </button>
       ) : (
-        <button
-          className="md:hidden fixed bottom-2 right-2 p-2 shadow-md rounded-full focus:outline-none focus:ring z-10 bg-white"
-          onClick={openModal}
-        >
-          <PlusCircleIcon className="w-8 h-8 text-green-400" />
-        </button>
+        <Portal>
+          <button
+            className="md:hidden absolute bottom-2 right-2 p-2 shadow-md rounded-full focus:outline-none focus:ring bg-white"
+            onClick={openModal}
+          >
+            <PlusCircleIcon className="w-8 h-8 text-green-400" />
+          </button>
+        </Portal>
       )}
       <Suspense fallback={null}>
         <AddModal isOpen={showModal} onClose={closeModal} />

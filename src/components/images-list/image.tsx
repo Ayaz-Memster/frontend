b6f6020fe7be07c8da9f8f416e7168b4edc527ca
Andrey Kiselev <omega-faworit@yandex.ru>
2021-08-01 13:20:57 +0300

    feat: update image

--- src/components/images-list/image.tsx
+++ src/components/images-list/image.tsx
@@ -10,6 +10,7 @@ export const Image = () => {
   const openZoomModal = useCallback(() => setIsZoomOpen(true), []);
   const closeZoomModal = useCallback(() => setIsZoomOpen(false), []);
 
+  const title = 'Corgi';
   const imgLink = useMemo(
     () =>
       `http://placecorgi.com/${300 + Math.floor(Math.random() * 100)}/${
@@ -21,51 +22,28 @@ export const Image = () => {
   return (
     <>
       <div className="grid place-items-center">
-        <article className="group first-letter:grid grid-flow-row items-center relative">
+        <article className="first-letter:grid grid-flow-row items-center relative">
           <figure>
-            <div className="w-[300px] h-[300px] overflow-hidden flex items-center justify-center hover:overflow-visible">
+            <div className="w-full sm:w-[300px] sm:h-[300px] overflow-hidden flex items-center justify-center">
               <img
                 src={imgLink}
                 width="300"
                 loading="lazy"
                 height="300"
                 alt="Corgi"
-                className="hover:cursor-zoom-in object-contain max-w-none"
-                onLoad={(event) => {
-                  const img = event.target as HTMLImageElement;
-                  const imgWidth = img.naturalWidth;
-                  const imgHeight = img.naturalHeight;
-                  if (imgWidth > imgHeight) {
-                    const ratio = 300 / imgHeight;
-                    const newWidth = imgWidth * ratio;
-                    img.width = newWidth;
-                    img.height = 300;
-                  } else {
-                    const ratio = 300 / imgWidth;
-                    const newHeight = imgHeight * ratio;
-                    img.width = 300;
-                    img.height = newHeight;
-                  }
-                }}
+                className="hover:cursor-zoom-in object-cover max-w-none"
                 onClick={openZoomModal}
               />
             </div>
-            <figcaption className="text-2xl text-center">Corgi</figcaption>
+            <figcaption className="text-2xl text-center">{title}</figcaption>
           </figure>
           <button
-            className="opacity-0 group-hover:opacity-100 absolute top-1 right-8 focus:opacity-100 focus:outline-none focus:ring rounded-full"
+            className="opacity-0 bg-white p-1 absolute top-2 right-2 focus:opacity-100 focus:outline-none focus:ring rounded-full"
             onClick={openZoomModal}
+            aria-label={`Zoom ${title} image`}
           >
             <ZoomInIcon className="w-7 h-7" />
           </button>
-          <a
-            href={imgLink}
-            target="_blank"
-            download={'Corgi.png'}
-            className="opacity-0 group-hover:opacity-100 absolute top-1 right-1 focus:opacity-100 focus:outline-none focus:ring rounded-full"
-          >
-            <DownloadIcon className="w-7 h-7" />
-          </a>
         </article>
       </div>
       <Suspense fallback={null}>

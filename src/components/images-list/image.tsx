648cd5f3a0c7c6c90cbda78ea4022aeea99beff2
Andrey Kiselev <omega-faworit@yandex.ru>
2021-08-14 18:20:31 +0300

    fix: move zoom button

--- src/components/images-list/image.tsx
+++ src/components/images-list/image.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useMemo, useState, lazy, Suspense } from 'react';
-import { ZoomInIcon, DownloadIcon } from '@heroicons/react/outline';
+import { ZoomInIcon } from '@heroicons/react/outline';
 import dayjs from 'dayjs';
 
 const ZoomModal = lazy(() => import('./zoom-modal'));
@@ -22,9 +22,9 @@ export const Image = () => {
   return (
     <>
       <div className="grid place-items-center">
-        <article className="w-full first-letter:grid grid-flow-row items-center relative">
+        <article className="w-full first-letter:grid grid-flow-row items-center">
           <figure className="w-full grid place-items-center">
-            <div className="w-full sm:w-[300px] sm:h-[300px] overflow-hidden flex items-center justify-center">
+            <div className="w-full sm:w-[300px] sm:h-[300px] overflow-hidden flex items-center justify-center relative">
               <img
                 src={imgLink}
                 width="300"
@@ -34,16 +34,16 @@ export const Image = () => {
                 className="hover:cursor-zoom-in object-cover max-w-none w-[95%] sm:w-auto"
                 onClick={openZoomModal}
               />
+              <button
+                className="opacity-0 bg-white p-1 absolute top-2 right-2 focus:opacity-100 focus:outline-none focus:ring rounded-full"
+                onClick={openZoomModal}
+                aria-label={`Zoom ${title} image`}
+              >
+                <ZoomInIcon className="w-7 h-7" />
+              </button>
             </div>
             <figcaption className="text-2xl text-center">{title}</figcaption>
           </figure>
-          <button
-            className="opacity-0 bg-white p-1 absolute top-2 right-2 focus:opacity-100 focus:outline-none focus:ring rounded-full"
-            onClick={openZoomModal}
-            aria-label={`Zoom ${title} image`}
-          >
-            <ZoomInIcon className="w-7 h-7" />
-          </button>
         </article>
       </div>
       <Suspense fallback={null}>

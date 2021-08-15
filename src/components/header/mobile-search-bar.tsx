3f50f8d3d0b50c4c378d24c7eb1a652e625314ac
Andrey Kiselev <omega-faworit@yandex.ru>
2021-08-16 00:05:37 +0300

    feat: update modile search

--- src/components/header/mobile-search-bar.tsx
+++ src/components/header/mobile-search-bar.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { ChevronUpIcon, XIcon } from '@heroicons/react/outline';
+import { useSearch } from '../../hooks/useImages';
 
 export interface MobileSearchBarProps {
   onClose: () => void;
 }
 
 export const MobileSearchBar = (props: MobileSearchBarProps) => {
+  const { query, setQuery } = useSearch();
+
   return (
-    <div className="flex gap-2 absolute top-full bg-white p-2 left-0 right-0 shadow-md z-10">
+    <div className="flex gap-2 absolute top-full bg-white p-2 left-0 right-0 shadow-md z-10 md:hidden">
       <div className="grid place-items-center">
         <button
           className="text-gray-400 rounded-full focus:outline-none focus:ring"
@@ -21,6 +24,8 @@ export const MobileSearchBar = (props: MobileSearchBarProps) => {
           placeholder="Search"
           autoFocus
           className="w-full h-full rounded-md p-1 focus:outline-none focus:ring text-2xl pr-6"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
         <button className="absolute right-1 top-1/2 -translate-y-1/2 text-gray-400 rounded-full focus:outline-none focus:ring">
           <XIcon className="w-6 h-6" />
